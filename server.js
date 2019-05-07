74c74dfd32596fc78a1bbfc01e2a633e23165e19
felipedelatorre <felipede_latorre@hotmail.com>
2019-05-07 08:51:50 -0700

    Listening Port and simple css

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//-----------------------------
+// Configs for server
+//------------------------------
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+// For rendering
+app.set('view engine', 'ejs');
+
+//-----------------------------
+// Practice
+//------------------------------
+
+let list = ['apples', 'celery', 'butter'];
+
+app.get('/', (request, response) => {
+  response.render('pages/index');
+});
+
+app.get('/error', (request, response) => {
+  response.render('pages/error', {arrayOfItems: list});
+});
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
